500fe76f1845d03ee8efa12fcef9c492884f3357
Paiva2 <joaovitor.paiva145@hotmail.com>
2023-07-22 16:44:20 -0300

    feat/home carousel desktop

--- src/components/HomeCarousel/index.tsx
+++ src/components/HomeCarousel/index.tsx
@@ -1,25 +1,28 @@
-import { useState } from "react"
-import { CarouselContent, CarouselPages, CarouselWrapper } from "./styles"
+import { useRef, useState } from "react"
+import {
+  CarouselContent,
+  CarouselPages,
+  CarouselTextsContainer,
+  CarouselTextsWrapper,
+  CarouselWrapper,
+} from "./styles"
 import { useKeenSlider, TrackDetails } from "keen-slider/react"
 import CarouselLinkArrow from "../../icons/CarouselLinkArrow"
-
-const images = [
-  "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-  "https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-  "https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-  "https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
-]
+import { carouselContents } from "./utils/carouselContents"
+import { motion } from "framer-motion"
 
 const HomeCarousel = () => {
   const [details, setDetails] = useState<TrackDetails | null>(null)
   const [currentSlide, setCurrentSlide] = useState(0)
   const [loaded, setLoaded] = useState(false)
+  const titleRef = useRef(0)
 
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: 0,
 
     slideChanged(slider) {
       setCurrentSlide(slider.track.details.rel)
+      triggerAnimation()
     },
 
     detailsChanged(s) {
@@ -31,7 +34,7 @@ const HomeCarousel = () => {
     },
   })
 
-  function scaleStyle(idx: number) {
+  function scaleTransitionEffect(idx: number) {
     if (!details) return {}
 
     const slide = details.slides[idx]
@@ -44,73 +47,36 @@ const HomeCarousel = () => {
     }
   }
 
+  const triggerAnimation = () => {
+    titleRef.current++
+  }
+
   return (
     <CarouselWrapper>
       <div ref={sliderRef} className="keen-slider zoom-out">
-        {images.map((src, idx) => (
-          <div key={idx} className="keen-slider__slide zoom-out__slide">
-            <CarouselContent style={scaleStyle(idx)}>
-              <img src={src} />
-
-              <div
-                style={{
-                  zIndex: "1000",
-                  width: "100%",
-                  alignItems: "center",
-                  height: "100%",
-                  display: "flex",
-                }}
-              >
-                <div
-                  style={{
-                    maxWidth: "700px",
-                    height: "100%",
-                    padding: "10.75rem 20px 0px 10.75rem",
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "20px",
-                    wordBreak: "break-word",
-                  }}
-                >
-                  <h1
-                    style={{
-                      fontSize: "80px",
-                      color: "#fefffd",
+        {carouselContents.map((content, idx) => (
+          <div key={content.id} className="keen-slider__slide zoom-out__slide">
+            <CarouselContent style={scaleTransitionEffect(idx)}>
+              <img src={content.image} />
+              <CarouselTextsContainer>
+                <CarouselTextsWrapper>
+                  <motion.div
+                    key={titleRef.current}
+                    initial={{ opacity: 0, x: -150 }}
+                    animate={{ x: 0, y: 0, opacity: 1 }}
+                    transition={{
+                      duration: 0.4,
+                      ease: "easeIn",
                     }}
                   >
-                    Project Paramour
-                  </h1>
-
-                  <p
-                    style={{
-                      fontSize: "1rem",
-                      color: "#fefffd",
-                      fontWeight: "500",
-                    }}
-                  >
-                    Project made for an art museum near Southwest London. Project
-                    Paramour is a statement of bold, modern architecture.
-                  </p>
-                  <a
-                    href="/"
-                    style={{
-                      all: "unset",
-                      backgroundColor: "#1c1d22",
-                      color: "#fefffd",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                      fontWeight: "700",
-                      width: "50%",
-                      padding: "25px 20px",
-                      cursor: "pointer",
-                      gap: "20px",
-                    }}
-                  >
-                    See Our Portfolio <CarouselLinkArrow />
-                  </a>
-                </div>
-              </div>
+                    <h1>{content.title}</h1>
+                    <p>{content.description}</p>
+                    <a>
+                      See Our Portfolio <CarouselLinkArrow />
+                    </a>
+                  </motion.div>
+                </CarouselTextsWrapper>
+              </CarouselTextsContainer>
             </CarouselContent>
           </div>
         ))}
@@ -125,7 +91,7 @@ const HomeCarousel = () => {
                   onClick={() => {
                     instanceRef.current?.moveToIdx(idx)
                   }}
-                  className={"dot" + (currentSlide === idx ? " active" : "")}
+                  className={currentSlide === idx ? " active" : ""}
                 >
                   0{idx + 1}
                 </button>
---
+++ src/components/HomeCarousel/utils/carouselContents.ts
@@ -0,0 +1,33 @@
+export const carouselContents = [
+  {
+    id: "1",
+    image: "https://i.postimg.cc/SxrcSvMx/image-hero-paramour.jpg",
+    title: "Project Paramour",
+    description:
+      "Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
+  },
+
+  {
+    id: "2",
+    image: "https://i.postimg.cc/V6StFsHJ/image-hero-seraph.jpg",
+    title: "Seraph Station",
+    description:
+      "The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
+  },
+
+  {
+    id: "3",
+    image: "https://i.postimg.cc/5tyQxNJs/image-hero-federal.jpg",
+    title: "Federal II Tower",
+    description:
+      "A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
+  },
+
+  {
+    id: "4",
+    image: "https://i.postimg.cc/7Y129WXn/image-hero-trinity.jpg",
+    title: "Trinity Bank Tower",
+    description:
+      "Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
+  },
+]
